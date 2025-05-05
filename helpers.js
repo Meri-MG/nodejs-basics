adaa77b89c68e288651e72ce50c29d8289b961aa
Meri-MG <mgogi13@freeuni.edu.ge>
2025-05-05 11:16:57 +0400

    feat: implement up, ls, cd commands

--- helpers.js
+++ helpers.js
@@ -1,4 +1,4 @@
-export function parseArgs(argv) {
+function parseArgs(argv) {
   const args = {};
   argv.slice(2).forEach(arg => {
     const [key, value] = arg.split('=');
@@ -7,6 +7,44 @@ export function parseArgs(argv) {
   return args;
 }
 
-export function logCurrentDirectory() {
-  console.log(`You are currently in ${process.cwd()}`);
+function logCurrentDirectory(currentDir) {
+  console.log(`You are currently in ${currentDir}`);
+}
+
+function goUp(path, currentDir, rootDir) {
+  const parentDir = path.dirname(currentDir);
+  if (parentDir.length < rootDir.length) {
+    return currentDir;
+  }
+  return parentDir;
+}
+
+function changeDir(fs, path, inputPath, currentDir) {
+  const resolvedPath = path.resolve(currentDir, inputPath);
+
+  if (fs.existsSync(resolvedPath) && fs.statSync(resolvedPath).isDirectory()) {
+    return resolvedPath;
+  } else {
+    console.log('Operation failed');
+    return currentDir;
+  }
+}
+
+async function listDir(currentDir) {
+  const fs = await import('node:fs/promises');
+  const entries = await fs.readdir(currentDir, { withFileTypes: true });
+  const result = entries.map(entry => ({
+    Name: entry.name,
+    Type: entry.isDirectory() ? 'directory' : 'file'
+  }));
+
+  console.table(result);
+}
+
+export {
+  parseArgs,
+  logCurrentDirectory,
+  goUp,
+  changeDir,
+  listDir
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import readline from 'node:readline';
-import { parseArgs, logCurrentDirectory } from './helpers.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { parseArgs, logCurrentDirectory, goUp, listDir, changeDir } from './helpers.js';
 
 const username = parseArgs(process.argv).username || 'Anonymous';
-console.log(`Welcome to the File Manager, ${username}!`);
-logCurrentDirectory();
+let currentDir = process.cwd();
+const rootDir = path.parse(currentDir).root;
+logCurrentDirectory(currentDir);
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,6 +23,31 @@ rl.on('line', async (line) => {
     console.log(`Thank you for using File Manager, ${username}, goodbye!`);
     process.exit(0);
   }
+
+  try {
+    const [command, ...args] = input.split(' ');
+
+    switch (command) {
+      case 'up':
+        currentDir = goUp(path, currentDir, rootDir)
+        break;
+
+      case 'cd':
+        currentDir = changeDir(fs, path, args[0], currentDir);
+        break;
+
+      case 'ls':
+        await listDir(currentDir);
+        break;
+
+      default:
+        console.log('Invalid input');
+    }
+  } catch (err) {
+    console.log('Operation failed');
+  }
+
+  logCurrentDirectory(currentDir);
   rl.prompt();
 });
 
@@ -28,4 +56,3 @@ rl.on('SIGINT', () => {
   console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
   process.exit(0);
 });
-rl
